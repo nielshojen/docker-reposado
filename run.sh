4b1c2938f36fb78461c4aa68d8a3482615b981c6
Niels Højen <niels@hojen.net>
2019-10-23 01:47:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,34 +10,34 @@ fi
 if [[ ${MIN_OS_CATALOGS} ]]; then
   /bin/echo "Setting AppleCatalogURLs to 10.${MIN_OS_CATALOGS}+"
   CATALOGS=$'\n  <key>AppleCatalogURLs</key>\n  <array>'
-  if [[ ${MIN_OS_CATALOG} -le 6 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 6 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/index.sucatalog</string>\n    <string>http://swscan.apple.com/content/catalogs/index-1.sucatalog</string>\n    <string>http://swscan.apple.com/content/catalogs/others/index-leopard.merged-1.sucatalog</string>\n    <string>http://swscan.apple.com/content/catalogs/others/index-leopard-snowleopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 7 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 7 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 8 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 8 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 9 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 9 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 10 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 10 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 11 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 11 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 12 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 12 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 13 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 13 ]]; then
     CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 14 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 14 ]]; then
     CATALOGS+=$'\n    <string>https://swscan.apple.com/content/catalogs/others/index-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
-  if [[ ${MIN_OS_CATALOG} -le 15 ]]; then
+  if [[ ${MIN_OS_CATALOGS} -le 15 ]]; then
     CATALOGS+=$'\n    <string>https://swscan.apple.com/content/catalogs/others/index-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
   fi
   CATALOGS+=$'\n  </array>'
