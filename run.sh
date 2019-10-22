ab803935c38407b9607589aa147bb0d0dbef13a5
Niels Højen <niels@hojen.net>
2019-10-23 01:25:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,24 +2,59 @@
 
 /usr/sbin/nginx
 
-if [[ $LOCAL_URL ]]; then
+if [[ ${LOCAL_URL} ]]; then
+  /bin/echo "Setting LocalCatalogURLBase to ${LOCAL_URL}"
+  LOCALCATALOGURLBASE=$'\n  <key>LocalCatalogURLBase</key>\n  <string>'${LOCAL_URL}'</string>'
+fi
 
-/bin/cat <<EOF > /reposado/code/preferences.plist
+if [[ ${MIN_OS_CATALOGS} ]]; then
+  /bin/echo "Setting AppleCatalogURLs to 10.${MIN_OS_CATALOGS}+"
+  CATALOGS=$'\n  <key>AppleCatalogURLs</key>\n  <array>'
+  if [[ ${MIN_OS_CATALOG} -le 6 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/index.sucatalog</string>\n    <string>http://swscan.apple.com/content/catalogs/index-1.sucatalog</string>\n    <string>http://swscan.apple.com/content/catalogs/others/index-leopard.merged-1.sucatalog</string>\n    <string>http://swscan.apple.com/content/catalogs/others/index-leopard-snowleopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 7 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 8 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 9 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 10 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 11 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 12 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 13 ]]; then
+    CATALOGS+=$'\n    <string>http://swscan.apple.com/content/catalogs/others/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 14 ]]; then
+    CATALOGS+=$'\n    <string>https://swscan.apple.com/content/catalogs/others/index-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  if [[ ${MIN_OS_CATALOG} -le 15 ]]; then
+    CATALOGS+=$'\n    <string>https://swscan.apple.com/content/catalogs/others/index-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog</string>'
+  fi
+  CATALOGS+=$'\n  </array>'
+fi
+
+/bin/cat <<EOF > ~/Downloads/preferences.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-<dict>
-  <key>LocalCatalogURLBase</key>
-  <string>${LOCAL_URL}</string>
+<dict>${LOCALCATALOGURLBASE}
   <key>UpdatesMetadataDir</key>
   <string>/reposado/metadata</string>
   <key>UpdatesRootDir</key>
-  <string>/reposado/html</string>
+  <string>/reposado/html</string>${CATALOGS}
 </dict>
 </plist>
 EOF
-
-fi
 
 for SCRIPT in $(/usr/bin/find /reposado/scripts -type f); do
 	/bin/bash $SCRIPT
